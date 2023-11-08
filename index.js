d76d8d18fa0b3e88ffce06ce9587cb281bfa55cf
shohaib1996 <khanshohaibhossain@gmail.com>
2023-11-08 12:09:22 +0600

    origin added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://monumental-medovik-3187d5.netlify.app"],
     credentials: true
 }))
 app.use(express.json())
